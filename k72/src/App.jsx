85d1197fe78ed370ae1dd11ac04566d253e48787
robycode06-cmd <your-email@example.com>
2026-05-29 09:58:59 +0530

    integrated tailwind

--- k72/src/App.jsx
+++ k72/src/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Agence from './pages/Agence';
+import Project from './pages/Project';
+
+
 const App = () => {
   return (
-    <div>App</div>
+    <div className='text-white bg-black w-full h-screen text-6xl'>
+      <h1>yo</h1>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/agence' element={<Agence/>}></Route>
+        <Route path='/projects' element={<Project/>}></Route>
+      </Routes>
+    </div>
   )
 }
 
